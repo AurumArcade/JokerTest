cdd64331f8c27734545a752e76c81b21d480bbca
Graham Taylor <gtaylor@nanthealth.com>
2022-07-19 20:00:38 +0100

    Test Oct Build

--- network.js
+++ network.js
@@ -1,5 +1,6 @@
 /*
-Used to set the network: https://chainlist.org/
+Used to set the network
+
 1 Mainnet
 3 Ropsten
 4 Rinkeby
@@ -13,13 +14,7 @@ Used to set the network: https://chainlist.org/
 80001 Matic Testnet
 43113 Avalanche Testnet
 43114 Avalanche Mainnet
-42220 Celo Mainnet
-44787 Celo Alfajores Testnet
-62320 Celo Baklava Testnet
-1666700000 Harmony Testnet Shard0
-1666600000 Harmony Mainnet Shard0
-25 Cronos Mainnet Beta
-338 Cronos Testnet
 */
 
-window.web3ChainId = 1;
+window.web3NetworkId = 1;
+window.web3NetworkName = "mainnet"
